412f03cbdae9e79aa42908525f09f8966b1826de
Mikusuo <m_suokas@outlook.com>
2019-01-29 09:26:51 +0200

    Toimiiko

--- Juna-aikataulut/Form1.cs
+++ Juna-aikataulut/Form1.cs
@@ -16,5 +16,10 @@ namespace Juna_aikataulut
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
